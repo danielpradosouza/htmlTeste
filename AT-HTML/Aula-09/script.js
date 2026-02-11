8071fb87b113c894e489e02f91b914eb040b1836
danielpradosouza <79987537+danielpradosouza@users.noreply.github.com>
2026-02-11 11:34:30 -0300

    porti

---
+++ AT-HTML/Aula-09/script.js
@@ -0,0 +1,13 @@
+
+
+let nome = "Daniel";
+let anoNasc = 1999;
+let anoAtual =2026;
+let idade = anoAtual - anoNasc;
+
+console.log("Olá meu nome ",nome," e tenho ",idade,"anos")
+
+if(idade>=18)
+    console.log("Maior de idade")
+else
+     console.log("Menor de idade")
