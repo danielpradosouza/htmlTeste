e0e781f59777c2afe34133ca8397cf3ac78bd561
danielpradosouza <79987537+danielpradosouza@users.noreply.github.com>
2026-03-26 11:12:13 -0300

    continu desafio

--- AT-HTML/desafio/script.js
+++ AT-HTML/desafio/script.js
@@ -14,3 +14,24 @@ function showConfirm() {
         acao.innerText = "Não confirmou a ação";
     }
 }
+function nomePrompt(){
+var nome =prompt("Digite o seu nome")
+acao.innerText=('Olá ' +nome)
+}
+function showLog(){
+    console.log("Mensagem no console!")
+    acao.innerText="Mensagem no console"
+}
+function nomeInput(){
+    let nomeInpu=document.getElementById('inome').value
+    acao.innerText=('Olá '+nomeInpu)
+    
+}
+function mudarTitulo(){
+    let mudaTitulo=document.getElementById('titulo').value
+    acao.innerText='Título alterado ✅'
+}
+function mudarTema(){
+    document.body.classList.toggle("tema-claro");
+   document.body.style.background = 'white';
+}
